0fde8a1e94e736c629a5a4707de50c7309949e2f
Rami Piik <rami.piik@gmail.com>
2022-01-25 18:16:27 +0200

    TDD ostoskori 1:  Luodun ostoskorin hinta ja tavaroiden määrä määrä on 0.

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,41 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        # pass
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self.kori=[]
+
+    def tavaroita_korissa(self):
+        # pass
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        count=0
+        for ostos in self.ostokset():
+            count+=ostos.lukumaara()
+        return count
+
+    def hinta(self):
+        return 0
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        # pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self.kori
+
