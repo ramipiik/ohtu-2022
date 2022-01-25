5151aea1325e0ec441014a6317067e77c83e5643
Rami Piik <rami.piik@gmail.com>
2022-01-25 19:10:38 +0200

    TDD-ostoskori step 8

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -46,3 +46,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(),2*3)
     
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
+ 
+        # testaa että metodin palauttaman listan pituus 1
+    
