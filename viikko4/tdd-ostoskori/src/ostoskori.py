81179b021ca27fabcf7f397b78b4865b857c3294
Rami Piik <rami.piik@gmail.com>
2022-01-25 18:41:47 +0200

    TDD-ostoskori 5: Kahden eri tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin tuotteiden hintojen summa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,12 +3,10 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        # pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
         self.kori=[]
 
     def tavaroita_korissa(self):
-        # pass
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -26,12 +24,10 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        # pass
         for ostos in self.ostokset():
             if ostos.tuotteen_nimi()==lisattava:
                 ostos.muuta_lukumaaraa(1)
                 return
-        
         self.kori.append(Ostos(lisattava))
         
 
@@ -44,7 +40,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        # pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
         return self.kori
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,17 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(),3)
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_tavaraa(self):
+        leipa = Tuote("Leipä", 3)
+        self.kori.lisaa_tuote(leipa)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
+    
+    def kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        leipa = Tuote("Leipä", 2)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(),5)
